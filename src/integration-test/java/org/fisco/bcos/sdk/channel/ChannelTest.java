d37a9f49975a31a7e8e1809d37fe3d232e052dfe
chaychen2005 <35914409+chaychen2005@users.noreply.github.com>
2020-07-29 17:39:07 +0800

    check connections in channel (#34)

--- src/integration-test/java/org/fisco/bcos/sdk/channel/ChannelTest.java
+++ src/integration-test/java/org/fisco/bcos/sdk/channel/ChannelTest.java
@@ -15,21 +15,33 @@
 
 package org.fisco.bcos.sdk.channel;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.netty.channel.ChannelHandlerContext;
+import org.fisco.bcos.sdk.channel.model.EnumChannelProtocolVersion;
+import org.fisco.bcos.sdk.channel.model.HeartBeatParser;
+import org.fisco.bcos.sdk.channel.model.NodeHeartbeat;
+import org.fisco.bcos.sdk.channel.model.Options;
 import org.fisco.bcos.sdk.model.Message;
 import org.fisco.bcos.sdk.model.MsgType;
+import org.fisco.bcos.sdk.model.Response;
 import org.fisco.bcos.sdk.network.MsgHandler;
+import org.fisco.bcos.sdk.utils.ChannelUtils;
+import org.fisco.bcos.sdk.utils.ObjectMapperFactory;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 import static org.junit.Assert.fail;
 
 public class ChannelTest {
+    private Logger logger = LoggerFactory.getLogger(ChannelImp.class);
+    private Channel channel;
+
     @Test
     public void testConnect() {
-        Logger logger = LoggerFactory.getLogger(ChannelImp.class);
-        Channel channel = Channel.build("src/integration-test/resources/config-example.yaml");
+        channel = Channel.build("src/integration-test/resources/config-example.yaml");
         class TestMsgHandler implements MsgHandler {
             @Override
             public void onConnect(ChannelHandlerContext ctx) {
@@ -50,11 +62,63 @@ public class ChannelTest {
         channel.addDisconnectHandler(testMsgHandler);
         try{
             channel.start();
+            sendMessage();
             Thread.sleep(10000);
             channel.stop();
         } catch (Exception e) {
             e.printStackTrace();
             fail("Exception is not expected");
         }
+    }
+
+    // use heart beat for case to send
+    private void sendMessage() {
+        List<String> peers =  channel.getAvailablePeer();
+        if (peers.size() == 0) {
+            fail("Empty available peer");
+        }
+        String host = peers.get(0);
+        Message message = new Message();
+        try {
+            message.setSeq(ChannelUtils.newSeq());
+            message.setResult(0);
+            message.setType(Short.valueOf((short) MsgType.CLIENT_HEARTBEAT.getType()));
+            HeartBeatParser heartBeatParser = new HeartBeatParser(EnumChannelProtocolVersion.VERSION_1);
+            message.setData(heartBeatParser.encode("0"));
+            logger.trace(
+                    "encodeHeartbeatToMessage, seq: {}, content: {}, messageType: {}",
+                    message.getSeq(),
+                    heartBeatParser.toString(),
+                    message.getType());
+        } catch (JsonProcessingException e) {
+            logger.error(
+                    "sendHeartbeatMessage failed for decode the message exception, errorMessage: {}",
+                    e.getMessage());
+            return;
+        }
+
+        ResponseCallback callback =
+                new ResponseCallback() {
+                    @Override
+                    public void onResponse(Response response) {
+                        try {
+                            NodeHeartbeat nodeHeartbeat =
+                                    ObjectMapperFactory.getObjectMapper()
+                                            .readValue(response.getContent(), NodeHeartbeat.class);
+                            int heartBeat = nodeHeartbeat.getHeartBeat();
+                            logger.trace(" heartbeat packet in ChannelTest, heartbeat is {} ", heartBeat);
+                            if (heartBeat != 1) {
+                                fail("heartbeat packet in ChannelTest fail");
+                            }
+                        } catch (Exception e) {
+                            fail(" channel protocol heartbeat failed, exception: " + e.getMessage());
+                        }
+                    }
+                };
+
+        logger.info(" test sendToPeer");
+        channel.sendToPeer(message, host);
+        logger.info(" test asyncSendToPeer");
+        channel.asyncSendToPeer(message, host, callback, new Options());
     }
 }
