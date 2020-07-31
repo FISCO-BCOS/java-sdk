ed014b92cfec3fed26a7dbcf6ba20727f952d5b5
cyjseagull <yujiechen_hust@163.com>
2020-07-31 14:22:20 +0800

    The client module add handlers to handle block number notify and transaction notify messages (#40)

--- src/main/java/org/fisco/bcos/sdk/channel/model/ChannelMessageError.java
+++ src/main/java/org/fisco/bcos/sdk/channel/model/ChannelMessageError.java
@@ -1,7 +1,13 @@
 package org.fisco.bcos.sdk.channel.model;
 
 public enum ChannelMessageError {
+    NODES_UNREACHABLE(99), // node unreachable
+    MESSAGE_SEND_EXCEPTION(100), // send failed after N times retry
     MESSAGE_TIMEOUT(102), // timeout
+    REJECT_AMOP_REQ_FOR_OVER_BANDWIDTHLIMIT(
+            103), // the AMOP_requests or the AMOP_multicast_requests have been rejected due to over
+    // bandwidth limit
+    MESSAGE_DECODE_ERROR(105), // decode error
     INTERNAL_MESSAGE_HANDLE_FAILED(-5000),
     CONNECTION_INVALID(-5001);
 
