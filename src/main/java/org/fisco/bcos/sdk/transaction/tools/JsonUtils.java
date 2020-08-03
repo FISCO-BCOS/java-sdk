2c68d0b64e5002ff3a8e168abc3a4d4161c692b1
dalaocu <njumjy06@126.com>
2020-08-03 15:33:35 +0800

    format

--- src/main/java/org/fisco/bcos/sdk/transaction/tools/JsonUtils.java
+++ src/main/java/org/fisco/bcos/sdk/transaction/tools/JsonUtils.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.fisco.bcos.sdk.transaction.model.exception.JsonException;
 import org.fisco.bcos.sdk.utils.ObjectMapperFactory;
 import org.slf4j.Logger;
