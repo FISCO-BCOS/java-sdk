a6bb1afba3a0a5c1482436cba4f702dcd511d2e8
maggie <hao.maggie.wu@gmail.com>
2020-09-03 16:38:07 +0800

    add amop tools, all tools finished

--- src/demo/java/org/fisco/bcos/sdk/demo/amop/tool/AmopSubscribePrivate.java
+++ src/demo/java/org/fisco/bcos/sdk/demo/amop/tool/AmopSubscribePrivate.java
@@ -3,7 +3,6 @@ package org.fisco.bcos.sdk.demo.amop.tool;
 import org.fisco.bcos.sdk.BcosSDK;
 import org.fisco.bcos.sdk.amop.Amop;
 import org.fisco.bcos.sdk.amop.AmopCallback;
-import org.fisco.bcos.sdk.amop.topic.AmopMsgIn;
 import org.fisco.bcos.sdk.crypto.keystore.KeyManager;
 import org.fisco.bcos.sdk.crypto.keystore.P12Manager;
 
@@ -11,6 +10,10 @@ public class AmopSubscribePrivate {
     private static String subscriberConfigFile =
             AmopSubscribe.class.getClassLoader().getResource("config-subscriber.toml").getPath();
 
+    /**
+     * @param args topic, privateKeyFile, password
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         if (args.length < 1) {
             System.out.println("Param: topic");
@@ -21,22 +24,7 @@ public class AmopSubscribePrivate {
         String password = args[2];
         BcosSDK sdk = new BcosSDK(subscriberConfigFile);
         Amop amop = sdk.getAmop();
-        AmopCallback cb =
-                new AmopCallback() {
-                    @Override
-                    public void receiveAmopMsg(AmopMsgIn msg) {
-                        byte[] responseData = "Yes, I received!".getBytes();
-                        System.out.println(
-                                "Step 2:Receive msg, topic:"
-                                        + msg.getTopic()
-                                        + " content:"
-                                        + new String(msg.getContent())
-                                        + " if not broadcast will response response:"
-                                        + new String(responseData));
-
-                        msg.sendResponse(responseData);
-                    }
-                };
+        AmopCallback cb = new DemoAmopCallback();
         System.out.println("Start test");
         amop.setCallback(cb);
         KeyManager km = new P12Manager(privateKeyFile, password);
