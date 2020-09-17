46b9d314bec96f7fa663efcbde9ea9cd223b2e9d
chaychen2005 <35914409+chaychen2005@users.noreply.github.com>
2020-09-17 15:33:52 +0800

    add blocknum check in event (#105)

--- src/main/java/org/fisco/bcos/sdk/eventsub/EventLogParams.java
+++ src/main/java/org/fisco/bcos/sdk/eventsub/EventLogParams.java
@@ -15,8 +15,11 @@
 
 package org.fisco.bcos.sdk.eventsub;
 
+import java.math.BigInteger;
 import java.util.List;
+import org.fisco.bcos.sdk.abi.tools.TopicTools;
 import org.fisco.bcos.sdk.utils.AddressUtils;
+import org.fisco.bcos.sdk.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -80,7 +83,7 @@ public class EventLogParams {
                 + "]";
     }
 
-    public boolean validAddresses() {
+    public boolean checkAddresses() {
         if (getAddresses() == null) {
             return false;
         }
@@ -93,8 +96,132 @@ public class EventLogParams {
         return true;
     }
 
-    public boolean valid() {
-        // todo add other verifications
-        return validAddresses();
+    /**
+     * check if topics valid
+     *
+     * @return
+     */
+    private boolean checkTopics() {
+
+        // topics
+        if ((getTopics() == null) || (getTopics().size() > TopicTools.MAX_NUM_TOPIC_EVENT_LOG)) {
+            return false;
+        }
+
+        for (Object topic : getTopics()) {
+            if (topic == null) {
+                continue;
+            }
+            if (topic instanceof String) {
+                // if valid topic
+                if (((String) topic).isEmpty()) {
+                    return false;
+                }
+            } else if (topic instanceof List) {
+                for (Object o : (List<String>) topic) {
+                    // if valid topic
+                    if (((String) o).isEmpty()) {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkToBlock(BigInteger blockNumber) {
+
+        // fromBlock="latest" but toBlock is not
+        // range in (latest,10), blockNumber 1 ok, 6 ok, 11 fail
+        BigInteger toBlock = new BigInteger(getToBlock());
+        return (blockNumber.compareTo(BigInteger.ONE) <= 0)
+                || (blockNumber.compareTo(BigInteger.ONE) > 0
+                        && (toBlock.compareTo(blockNumber)) > 0);
+    }
+
+    private boolean checkFromBlock(BigInteger blockNumber) {
+
+        // toBlock="latest" but fromBlock is not
+        // range in (10,latest), blockNumber 6 ok, 11 fail
+        BigInteger fromBlock = new BigInteger(getFromBlock());
+        // fromBlock is bigger than block number of the blockchain
+        if (fromBlock.compareTo(BigInteger.ZERO) <= 0) {
+            return false;
+        }
+
+        if (blockNumber.compareTo(BigInteger.ONE) > 0 && (fromBlock.compareTo(blockNumber) > 0)) {
+            logger.info(
+                    " future block range request, from: {}, to: {}", getFromBlock(), getToBlock());
+        }
+
+        return true;
+    }
+
+    private boolean checkFromToBlock(BigInteger blockNumber) throws NumberFormatException {
+
+        // fromBlock and toBlock none is "latest"
+        // range in (10,20), blockNumber 6 ok, 11 ok, 25 ok
+        BigInteger fromBlock = new BigInteger(getFromBlock());
+        BigInteger toBlock = new BigInteger(getToBlock());
+
+        if ((fromBlock.compareTo(BigInteger.ZERO) <= 0) || (fromBlock.compareTo(toBlock) > 0)) {
+            return false;
+        } else {
+            if (blockNumber.compareTo(BigInteger.ONE) > 0
+                    && (fromBlock.compareTo(blockNumber) > 0)) {
+                logger.info(
+                        " future block range request, from: {}, to: {}",
+                        getFromBlock(),
+                        getToBlock());
+            }
+            return true;
+        }
+    }
+
+    /**
+     * check if valid fromBlock and toBlock
+     *
+     * @param blockNumber
+     * @return
+     */
+    private boolean checkBlockRange(BigInteger blockNumber) {
+
+        if (StringUtils.isEmpty(getFromBlock()) || StringUtils.isEmpty(getToBlock())) {
+            return false;
+        }
+
+        boolean isValidBlockRange = true;
+
+        try {
+            if (getFromBlock().equals(TopicTools.LATEST)
+                    && !getToBlock().equals(TopicTools.LATEST)) {
+                // fromBlock="latest" but toBlock is not
+                isValidBlockRange = checkToBlock(blockNumber);
+            } else if (!getFromBlock().equals(TopicTools.LATEST)
+                    && getToBlock().equals(TopicTools.LATEST)) {
+                // toBlock="latest" but fromBlock is not
+                isValidBlockRange = checkFromBlock(blockNumber);
+            } else if (!getFromBlock().equals(TopicTools.LATEST)
+                    && !getToBlock().equals(TopicTools.LATEST)) {
+                isValidBlockRange = checkFromToBlock(blockNumber);
+            }
+        } catch (Exception e) {
+            // invalid blockNumber format string
+            isValidBlockRange = false;
+        }
+
+        return isValidBlockRange;
+    }
+
+    /**
+     * @param blockNumber: block number of blockchain
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public boolean checkParams(BigInteger blockNumber) {
+        return checkBlockRange(blockNumber) && checkAddresses() && checkTopics();
     }
 }
