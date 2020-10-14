0b3f40e227840a153fe169cd478d84ae1eff5465
Maggie WU <maggie@Maggies-MacBook-Pro.local>
2020-10-14 15:31:45 +0800

    add environment for amop demo

--- sdk-demo/src/main/java/org/fisco/bcos/sdk/demo/amop/tool/AmopSubscriber.java
+++ sdk-demo/src/main/java/org/fisco/bcos/sdk/demo/amop/tool/AmopSubscriber.java
@@ -1,23 +1,23 @@
 package org.fisco.bcos.sdk.demo.amop.tool;
 
+import java.net.URL;
 import org.fisco.bcos.sdk.BcosSDK;
 import org.fisco.bcos.sdk.amop.Amop;
 import org.fisco.bcos.sdk.amop.AmopCallback;
 
 public class AmopSubscriber {
-    private static String subscriberConfigFile =
-            AmopSubscriber.class
-                    .getClassLoader()
-                    .getResource("amop/config-subscriber-for-test.toml")
-                    .getPath();
 
     public static void main(String[] args) throws Exception {
+        URL configUrl =
+                AmopSubscriber.class
+                        .getClassLoader()
+                        .getResource("amop/config-subscriber-for-test.toml");
         if (args.length < 1) {
             System.out.println("Param: topic");
             return;
         }
         String topic = args[0];
-        BcosSDK sdk = BcosSDK.build(subscriberConfigFile);
+        BcosSDK sdk = BcosSDK.build(configUrl.getPath());
         Amop amop = sdk.getAmop();
         AmopCallback cb = new DemoAmopCallback();
         System.out.println("Start test");
