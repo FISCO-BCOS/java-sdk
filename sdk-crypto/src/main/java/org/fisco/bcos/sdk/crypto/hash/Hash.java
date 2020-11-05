9cb18d606e02697709f823823882b59ed7369336
cyjseagull <yujiechen_hust@163.com>
2020-11-05 19:17:54 +0800

    convert the input bytes data into hex string before calling sign

--- sdk-crypto/src/main/java/org/fisco/bcos/sdk/crypto/hash/Hash.java
+++ sdk-crypto/src/main/java/org/fisco/bcos/sdk/crypto/hash/Hash.java
@@ -18,5 +18,7 @@ package org.fisco.bcos.sdk.crypto.hash;
 public interface Hash {
     String hash(final String inputData);
 
+    String hashBytes(byte[] inputBytes);
+
     byte[] hash(final byte[] inputBytes);
 }
