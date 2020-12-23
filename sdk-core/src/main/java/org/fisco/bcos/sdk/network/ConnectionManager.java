c8357c23452501d298dad9b871c0efd84e831dae
cyjseagull <yujiechen_hust@163.com>
2020-12-23 11:39:34 +0800

    support jdk 15

--- sdk-core/src/main/java/org/fisco/bcos/sdk/network/ConnectionManager.java
+++ sdk-core/src/main/java/org/fisco/bcos/sdk/network/ConnectionManager.java
@@ -209,6 +209,7 @@ public class ConnectionManager {
     private SslContext initSslContext(ConfigOption configOption) throws NetworkException {
         try {
             Security.setProperty("jdk.disabled.namedCurves", "");
+            System.setProperty("jdk.sunec.disableNative", "false");
             // Get file, file existence is already checked when check config file.
             FileInputStream caCert =
                     new FileInputStream(
