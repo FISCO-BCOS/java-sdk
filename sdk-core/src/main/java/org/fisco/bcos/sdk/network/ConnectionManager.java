f0befcbc4365344c09cabcf6fa0dd6a90284dc16
chaychen2005 <35914409+chaychen2005@users.noreply.github.com>
2020-12-30 15:59:13 +0800

    add contructor in ConnectionManager (#237)

--- sdk-core/src/main/java/org/fisco/bcos/sdk/network/ConnectionManager.java
+++ sdk-core/src/main/java/org/fisco/bcos/sdk/network/ConnectionManager.java
@@ -76,11 +76,20 @@ public class ConnectionManager {
     private ScheduledExecutorService reconnSchedule = new ScheduledThreadPoolExecutor(1);
 
     public ConnectionManager(ConfigOption configOption, MsgHandler msgHandler) {
-        for (String peerIpPort : configOption.getNetworkConfig().getPeers()) {
-            connectionInfoList.add(new ConnectionInfo(peerIpPort));
+        this(configOption.getNetworkConfig().getPeers(), msgHandler);
+    }
+
+    public ConnectionManager(List<String> ipList, MsgHandler msgHandler) {
+        if (ipList != null) {
+            for (String peerIpPort : ipList) {
+                connectionInfoList.add(new ConnectionInfo(peerIpPort));
+            }
         }
         channelHandler = new ChannelHandler(this, msgHandler);
-        logger.info(" all connections: {}", connectionInfoList);
+        logger.info(
+                " all connections, size: {}, list: {}",
+                connectionInfoList.size(),
+                connectionInfoList);
     }
 
     public void startConnect(ConfigOption configOption) throws NetworkException {
