c712221b9b96fe9f6b5ab553d514ab646d7dc205
cyjseagull <yujiechen_hust@163.com>
2021-02-19 17:09:53 +0800

    fix java.security.keypair is null when new CryptoKeyPair randomly (#273)

--- sdk-crypto/src/main/java/org/fisco/bcos/sdk/crypto/keypair/ECDSAKeyPair.java
+++ sdk-crypto/src/main/java/org/fisco/bcos/sdk/crypto/keypair/ECDSAKeyPair.java
@@ -19,6 +19,7 @@ import java.math.BigInteger;
 import java.security.KeyPair;
 import org.fisco.bcos.sdk.crypto.hash.Hash;
 import org.fisco.bcos.sdk.crypto.hash.Keccak256;
+import org.fisco.bcos.sdk.crypto.keystore.KeyTool;
 import org.fisco.bcos.sdk.utils.Hex;
 import org.fisco.bcos.sdk.utils.Numeric;
 
@@ -30,6 +31,7 @@ public class ECDSAKeyPair extends CryptoKeyPair {
         CryptoKeyPair keyPair = this.generateKeyPair();
         this.hexPrivateKey = keyPair.getHexPrivateKey();
         this.hexPublicKey = keyPair.getHexPublicKey();
+        this.keyPair = KeyTool.convertHexedStringToKeyPair(this.hexPrivateKey, curveName);
     }
 
     public ECDSAKeyPair(KeyPair javaKeyPair) {
@@ -40,6 +42,7 @@ public class ECDSAKeyPair extends CryptoKeyPair {
     protected ECDSAKeyPair(final CryptoResult ecKeyPairInfo) {
         super(ecKeyPairInfo);
         initECDSAKeyPair();
+        this.keyPair = KeyTool.convertHexedStringToKeyPair(this.hexPrivateKey, curveName);
     }
 
     private void initECDSAKeyPair() {
