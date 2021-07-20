322b27927d9b3d1c474426f6111f2e8d88fe7dcc
bxq2011hust <bxq2011hust@qq.com>
2021-07-20 11:03:12 +0800

    add webscoket

---
+++ sdk-core/src/main/java/org/fisco/bcos/sdk/network/WebSocketConnection.java
@@ -0,0 +1,51 @@
+package org.fisco.bcos.sdk.network;
+
+import org.fisco.bcos.sdk.channel.ResponseCallback;
+
+import java.io.IOException;
+
+public class WebSocketConnection implements Connection{
+
+    public WebSocketConnection(){}
+
+    /** @description close connection */
+    public void close(){}
+
+    /**
+     * @description connect to node
+     * @return true if connected
+     */
+    public Boolean connect(){
+
+    }
+
+    /**
+     * call rpc method
+     *
+     * @param request jsonrpc format string
+     * @return response
+     * @throws IOException
+     */
+    public String callMethod(String request) throws IOException{
+
+    }
+
+    public String getUri(){
+
+    }
+
+    /**
+     * Send a message to a node in the group and select the node with the highest block height in
+     * the group
+     *
+     * @param request The request to be sent
+     * @param callback callback to be called after receiving response
+     */
+    public void asyncCallMethod(String request, ResponseCallback callback) throws IOException{
+
+    }
+
+    public asyncSendMessage(byte[] message, ResponseCallback callback){
+
+    }
+}
