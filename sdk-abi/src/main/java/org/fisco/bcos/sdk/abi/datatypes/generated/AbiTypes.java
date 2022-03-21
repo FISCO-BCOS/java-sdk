b977c65542082b33d49b2a028bd078dfc213301a
dwzhan <dwzhan@iflytek.com>
2022-03-21 14:40:43 +0800

    feature-#248, Solidity转java代码支持struct等复杂数据类型

--- sdk-abi/src/main/java/org/fisco/bcos/sdk/abi/datatypes/generated/AbiTypes.java
+++ sdk-abi/src/main/java/org/fisco/bcos/sdk/abi/datatypes/generated/AbiTypes.java
@@ -3,6 +3,7 @@ package org.fisco.bcos.sdk.abi.datatypes.generated;
 import org.fisco.bcos.sdk.abi.datatypes.Address;
 import org.fisco.bcos.sdk.abi.datatypes.Bool;
 import org.fisco.bcos.sdk.abi.datatypes.DynamicBytes;
+import org.fisco.bcos.sdk.abi.datatypes.Type;
 import org.fisco.bcos.sdk.abi.datatypes.Utf8String;
 
 /**
@@ -219,7 +220,29 @@ public final class AbiTypes {
             case "bytes32":
                 return Bytes32.class;
             default:
-                throw new UnsupportedOperationException("Unsupported type encountered: " + type);
+                {
+                    try {
+                        return (Class<? extends Type>) Class.forName(type);
+                    } catch (ClassNotFoundException e) {
+                        throw new UnsupportedOperationException(
+                                "Unsupported type encountered: " + type);
+                    }
+                }
+        }
+    }
+
+    /**
+     * Returns the provided class type as a string. In case of a struct, it will return the struct
+     * name. For the tuple notation of a struct, example ((string,uint256)), think of taking an
+     * instance of the struct and calling the <code>instance.getTypeAsString()</code> method.
+     */
+    public static String getTypeAString(Class<? extends Type> type) {
+        if (Utf8String.class.equals(type)) {
+            return "string";
+        } else if (DynamicBytes.class.equals(type)) {
+            return "bytes";
+        } else {
+            return type.getSimpleName().toLowerCase();
         }
     }
 }
