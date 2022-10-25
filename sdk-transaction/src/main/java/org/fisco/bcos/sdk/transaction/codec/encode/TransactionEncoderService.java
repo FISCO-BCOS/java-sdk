e3f627da689457c41c5c32cb8631fa5e0f88f540
yangfang2 <yangfang2@iflytek.com>
2022-10-25 10:24:27 +0800

    transactionReceipt add encoder method

--- sdk-transaction/src/main/java/org/fisco/bcos/sdk/transaction/codec/encode/TransactionEncoderService.java
+++ sdk-transaction/src/main/java/org/fisco/bcos/sdk/transaction/codec/encode/TransactionEncoderService.java
@@ -16,10 +16,12 @@ package org.fisco.bcos.sdk.transaction.codec.encode;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.fisco.bcos.sdk.channel.model.EnumNodeVersion;
 import org.fisco.bcos.sdk.crypto.CryptoSuite;
 import org.fisco.bcos.sdk.crypto.keypair.CryptoKeyPair;
 import org.fisco.bcos.sdk.crypto.signature.Signature;
 import org.fisco.bcos.sdk.crypto.signature.SignatureResult;
+import org.fisco.bcos.sdk.model.TransactionReceipt;
 import org.fisco.bcos.sdk.rlp.RlpEncoder;
 import org.fisco.bcos.sdk.rlp.RlpList;
 import org.fisco.bcos.sdk.rlp.RlpString;
@@ -79,6 +81,21 @@ public class TransactionEncoderService implements TransactionEncoderInterface {
         return RlpEncoder.encode(rlpList);
     }
 
+    @Override
+    public byte[] encodeTransactionReceipt(
+            TransactionReceipt transactionReceipt, EnumNodeVersion.Version version) {
+        List<RlpType> values = asRlpValues(transactionReceipt, version);
+        RlpList rlpList = new RlpList(values);
+        return RlpEncoder.encode(rlpList);
+    }
+
+    @Override
+    public String encodeTransactionReceiptHexString(
+            TransactionReceipt transactionReceipt, EnumNodeVersion.Version version) {
+        byte[] rlpBytes = encodeTransactionReceipt(transactionReceipt, version);
+        return Numeric.toHexString(rlpBytes);
+    }
+
     /**
      * Rlp encode and sign based on RawTransaction
      *
@@ -121,6 +138,58 @@ public class TransactionEncoderService implements TransactionEncoderInterface {
         if (signatureResult != null) {
             result.addAll(signatureResult.encode());
         }
+        return result;
+    }
+
+    /**
+     * Rlp encode and sign based on TransactionReceipt
+     *
+     * @param transactionReceipt data to be encoded
+     * @param version version
+     * @return encoded & signed transactionReceipt RLP values
+     */
+    private static List<RlpType> asRlpValues(
+            TransactionReceipt transactionReceipt, EnumNodeVersion.Version version) {
+        List<RlpType> result = new ArrayList<>();
+        // bytes
+        result.add(RlpString.create(Numeric.hexStringToByteArray(transactionReceipt.getRoot())));
+
+        // BigInteger
+        result.add(RlpString.create(Numeric.toBigInt(transactionReceipt.getGasUsed())));
+
+        result.add(
+                RlpString.create(
+                        Numeric.hexStringToByteArray(transactionReceipt.getContractAddress())));
+
+        result.add(
+                RlpString.create(Numeric.hexStringToByteArray(transactionReceipt.getLogsBloom())));
+
+        result.add(RlpString.create(Numeric.toBigInt(transactionReceipt.getStatus())));
+
+        result.add(RlpString.create(Numeric.hexStringToByteArray(transactionReceipt.getOutput())));
+        // gas used
+        if (version != null && version.getMinor() >= 9) {
+            result.add(RlpString.create(Numeric.toBigInt(transactionReceipt.getRemainGas())));
+        }
+        // List
+        List<TransactionReceipt.Logs> logs = transactionReceipt.getLogs();
+        List<RlpType> logList = new ArrayList<>();
+        for (TransactionReceipt.Logs log : logs) {
+            List<RlpType> logUnit = new ArrayList<>();
+            logUnit.add(RlpString.create(Numeric.hexStringToByteArray(log.getAddress())));
+
+            List<String> topics = log.getTopics();
+            List<RlpType> topicList = new ArrayList<>();
+            for (String topic : topics) {
+                topicList.add(RlpString.create(Numeric.hexStringToByteArray(topic)));
+            }
+            RlpList topicRlpList = new RlpList(topicList);
+            logUnit.add(topicRlpList);
+            logUnit.add(RlpString.create(Numeric.hexStringToByteArray(log.getData())));
+            logList.add(new RlpList(logUnit));
+        }
+        RlpList logRlpList = new RlpList(logList);
+        result.add(logRlpList);
         return result;
     }
 
