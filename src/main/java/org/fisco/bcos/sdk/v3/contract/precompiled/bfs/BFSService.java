a86640be6e8c5418f7fa772ae623a5b478afde45
kyonRay <rui.kwok@outlook.com>
2022-11-03 22:37:05 +0800

    <feat>(auth): add account abolish status flag.

--- src/main/java/org/fisco/bcos/sdk/v3/contract/precompiled/bfs/BFSService.java
+++ src/main/java/org/fisco/bcos/sdk/v3/contract/precompiled/bfs/BFSService.java
@@ -2,6 +2,7 @@ package org.fisco.bcos.sdk.v3.contract.precompiled.bfs;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.fisco.bcos.sdk.v3.client.Client;
 import org.fisco.bcos.sdk.v3.codec.datatypes.DynamicArray;
 import org.fisco.bcos.sdk.v3.codec.datatypes.generated.tuples.generated.Tuple2;
@@ -62,12 +63,43 @@ public class BFSService {
         return listOutput.getValue2().getValue();
     }
 
+    public List<BFSInfo> listBFSInfo(String path) throws ContractException {
+        Tuple2<BigInteger, DynamicArray<BFSPrecompiled.BfsInfo>> listOutput =
+                bfsPrecompiled.list(path);
+        if (!listOutput.getValue1().equals(BigInteger.ZERO)) {
+            RetCode precompiledResponse =
+                    PrecompiledRetCode.getPrecompiledResponse(
+                            listOutput.getValue1().intValue(), "");
+            throw new ContractException(
+                    "BfsService: list return error code: "
+                            + listOutput.getValue1()
+                            + ", error msg: "
+                            + precompiledResponse.getMessage(),
+                    precompiledResponse.getCode());
+        }
+        return listOutput.getValue2().getValue().stream()
+                .map(BFSInfo::fromPrecompiledBfs)
+                .collect(Collectors.toList());
+    }
+
     public Tuple2<BigInteger, List<BFSPrecompiled.BfsInfo>> list(
             String absolutePath, BigInteger offset, BigInteger limit) throws ContractException {
         PrecompiledVersionCheck.LS_PAGE_VERSION.checkVersion(currentVersion);
         Tuple2<BigInteger, DynamicArray<BFSPrecompiled.BfsInfo>> listOutput =
                 bfsPrecompiled.list(absolutePath, offset, limit);
         return new Tuple2<>(listOutput.getValue1(), listOutput.getValue2().getValue());
+    }
+
+    public Tuple2<BigInteger, List<BFSInfo>> listBFSInfo(
+            String absolutePath, BigInteger offset, BigInteger limit) throws ContractException {
+        PrecompiledVersionCheck.LS_PAGE_VERSION.checkVersion(currentVersion);
+        Tuple2<BigInteger, DynamicArray<BFSPrecompiled.BfsInfo>> listOutput =
+                bfsPrecompiled.list(absolutePath, offset, limit);
+        return new Tuple2<>(
+                listOutput.getValue1(),
+                listOutput.getValue2().getValue().stream()
+                        .map(BFSInfo::fromPrecompiledBfs)
+                        .collect(Collectors.toList()));
     }
 
     public RetCode link(String name, String version, String contractAddress, String abi)
