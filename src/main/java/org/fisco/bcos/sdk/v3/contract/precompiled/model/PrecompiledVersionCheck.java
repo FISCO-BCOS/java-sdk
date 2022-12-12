2eeb1c1c9fe458ea63a732a066d1383d0d00abe2
root <root@server-a91f6db7-e71c-47cb-98f7-3ac704f032ce.novalocal>
2022-12-12 11:07:44 +0800

    Update TableInfo and Condition

--- src/main/java/org/fisco/bcos/sdk/v3/contract/precompiled/model/PrecompiledVersionCheck.java
+++ src/main/java/org/fisco/bcos/sdk/v3/contract/precompiled/model/PrecompiledVersionCheck.java
@@ -5,4 +5,5 @@ public class PrecompiledVersionCheck {
     public static final Version LINK_SIMPLE_VERSION = new Version("link", "3.1.0");
     public static final Version SET_CONTRACT_STATUS_VERSION =
             new Version("setContractStatus", "3.2.0");
+    public static final Version V320_CRUD_VERSION = new Version("v320CrudVersion", "3.2.0");
 }
