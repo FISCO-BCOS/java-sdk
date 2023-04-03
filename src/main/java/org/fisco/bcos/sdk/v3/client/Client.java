901e85c19212b7007fc0c6e06ac7e998612c0e71
Kyon <32325790+kyonRay@users.noreply.github.com>
2023-04-03 11:05:36 +0800

    <fix>(CI): add compatibility node test. (#752)

--- src/main/java/org/fisco/bcos/sdk/v3/client/Client.java
+++ src/main/java/org/fisco/bcos/sdk/v3/client/Client.java
@@ -42,6 +42,7 @@ import org.fisco.bcos.sdk.v3.client.protocol.response.SystemConfig;
 import org.fisco.bcos.sdk.v3.client.protocol.response.TotalTransactionCount;
 import org.fisco.bcos.sdk.v3.config.ConfigOption;
 import org.fisco.bcos.sdk.v3.crypto.CryptoSuite;
+import org.fisco.bcos.sdk.v3.model.EnumNodeVersion;
 import org.fisco.bcos.sdk.v3.model.callback.RespCallback;
 import org.fisco.bcos.sdk.v3.model.callback.TransactionCallback;
 import org.slf4j.Logger;
@@ -867,6 +868,20 @@ public interface Client {
     void getGroupNodeInfoAsync(String node, RespCallback<BcosGroupNodeInfo> callback);
 
     // ------------------------- rpc interface end ------------------------------------------
+
+    /**
+     * get the chain compatibility version
+     *
+     * @return the chain compatibility version
+     */
+    EnumNodeVersion.Version getChainVersion();
+
+    /**
+     * async get the chain compatibility version
+     *
+     * @param versionRespCallback the callback instance
+     */
+    void getChainVersionAsync(RespCallback<EnumNodeVersion.Version> versionRespCallback);
 
     void start();
 
