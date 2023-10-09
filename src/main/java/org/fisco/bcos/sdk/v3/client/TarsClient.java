004f710cbf2b5a6fe62ce4962a41012a6f8036a5
MO NAN <651932351@qq.com>
2023-10-09 18:49:17 +0800

    Support jni inside jar (#826)
    * Update sdk usage
    
    * Support jni inside jar

--- src/main/java/org/fisco/bcos/sdk/v3/client/TarsClient.java
+++ src/main/java/org/fisco/bcos/sdk/v3/client/TarsClient.java
@@ -1,12 +1,16 @@
 package org.fisco.bcos.sdk.v3.client;
 
+import java.io.File;
+import java.io.InputStream;
 import java.math.BigInteger;
-import java.net.URL;
+import java.nio.file.Files;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import org.fisco.bcos.sdk.tars.Callback;
@@ -90,8 +94,11 @@ public class TarsClient extends ClientImpl implements Client {
         this.transactionFactory = transactionFactory;
     }
 
-    protected TarsClient(String groupID, ConfigOption configOption, long nativePointer) {
+    protected TarsClient(String groupID, ConfigOption configOption, long nativePointer)
+            throws Exception {
         super(groupID, configOption, nativePointer);
+
+        loadLibrary();
         String connectionString =
                 RPCClient.toConnectionString(
                         new StringVector(configOption.getNetworkConfig().getTarsPeers()));
@@ -133,16 +140,40 @@ public class TarsClient extends ClientImpl implements Client {
                 };
     }
 
-    public static void loadLibrary() {
-        URL configUrl = TarsClient.class.getClassLoader().getResource(libFileName);
-        System.load(configUrl.getPath());
+    private static AtomicBoolean loaded = new AtomicBoolean(false);
+
+    private static void loadLibrary() throws Exception {
+        boolean inited = loaded.getAndSet(true);
+        if (inited) {
+            return;
+        }
+        try {
+            File jniFile = File.createTempFile(libFileName, UUID.randomUUID().toString());
+            String osName = System.getProperty("os.name");
+            if (osName.contains("Linux")) osName = "linux";
+            else if (osName.contains("Mac OS X")) osName = "darwin";
+            else if (osName.contains("Windows")) osName = "windows";
+
+            String osArch = System.getProperty("os.arch");
+            if (osArch.contains("amd64")) osArch = "x86_64";
+
+            InputStream jniStream =
+                    TarsClient.class.getResourceAsStream(
+                            "/" + osName + "-" + osArch + "/" + libFileName);
+            Files.copy(
+                    jniStream,
+                    jniFile.getAbsoluteFile().toPath(),
+                    java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+            System.load(jniFile.getAbsolutePath());
+            jniFile.deleteOnExit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 
-    public static void loadLibrary(String libPath) {
-        System.load(libPath);
-    }
-
-    public static TarsClient build(String groupId, ConfigOption configOption, long nativePointer) {
+    public static TarsClient build(String groupId, ConfigOption configOption, long nativePointer)
+            throws Exception {
         logger.info(
                 "TarsClient build, groupID: {}, configOption: {}, nativePointer: {}",
                 groupId,
