58424ecffc20c609cbc4795c2257e3402a5ec5d5
JimmyShi22 <417711026@qq.com>
2023-11-15 14:23:20 +0800

    Update version and feature

--- src/main/java/org/fisco/bcos/sdk/v3/contract/precompiled/sysconfig/SystemConfigFeature.java
+++ src/main/java/org/fisco/bcos/sdk/v3/contract/precompiled/sysconfig/SystemConfigFeature.java
@@ -5,13 +5,22 @@ import org.fisco.bcos.sdk.v3.model.EnumNodeVersion;
 public class SystemConfigFeature {
     public enum Features {
         BUGFIX_REVERT("bugfix_revert", EnumNodeVersion.BCOS_3_2_3.getVersion()),
+        BUGFIX_STATESTORAGE_HASH(
+                "bugfix_statestorage_hash", EnumNodeVersion.BCOS_3_2_4.getVersion()),
+        BUGFIX_EVM_CREATE2_DELEGATECALL_STATICCALL_CODECOPY(
+                "bugfix_evm_create2_delegatecall_staticcall_codecopy",
+                EnumNodeVersion.BCOS_3_2_4.getVersion()),
         FEATURE_SHARDING("feature_sharding", EnumNodeVersion.BCOS_3_5_0.getVersion()),
         FEATURE_RPBFT("feature_rpbft", EnumNodeVersion.BCOS_3_5_0.getVersion()),
         FEATURE_RPBFT_EPOCH_BLOCK_NUM(
                 "feature_rpbft_epoch_block_num", EnumNodeVersion.BCOS_3_5_0.getVersion()),
         FEATURE_RPBFT_EPOCH_SEALER_NUM(
                 "feature_rpbft_epoch_sealer_num", EnumNodeVersion.BCOS_3_5_0.getVersion()),
-        FEATURE_PAILLIER("feature_paillier", EnumNodeVersion.BCOS_3_5_0.getVersion());
+        FEATURE_PAILLIER("feature_paillier", EnumNodeVersion.BCOS_3_5_0.getVersion()),
+        FEATURE_BALANCE("feature_balance", EnumNodeVersion.BCOS_3_6_0.getVersion()),
+        FEATURE_BALANCE_PRECOMPILED(
+                "feature_balance_precompiled", EnumNodeVersion.BCOS_3_6_0.getVersion()),
+        FEATURE_BALANCE_POLICY1("feature_balance_policy1", EnumNodeVersion.BCOS_3_6_0.getVersion());
 
         private final String featureName;
         private final int enableVersion;
@@ -35,6 +44,10 @@ public class SystemConfigFeature {
         switch (name) {
             case "bugfix_revert":
                 return Features.BUGFIX_REVERT;
+            case "bugfix_statestorage_hash":
+                return Features.BUGFIX_STATESTORAGE_HASH;
+            case "bugfix_evm_create2_delegatecall_staticcall_codecopy":
+                return Features.BUGFIX_EVM_CREATE2_DELEGATECALL_STATICCALL_CODECOPY;
             case "feature_sharding":
                 return Features.FEATURE_SHARDING;
             case "feature_rpbft":
@@ -45,6 +58,12 @@ public class SystemConfigFeature {
                 return Features.FEATURE_RPBFT_EPOCH_SEALER_NUM;
             case "feature_paillier":
                 return Features.FEATURE_PAILLIER;
+            case "feature_balance":
+                return Features.FEATURE_BALANCE;
+            case "feature_balance_precompiled":
+                return Features.FEATURE_BALANCE_PRECOMPILED;
+            case "feature_balance_policy1":
+                return Features.FEATURE_BALANCE_POLICY1;
             default:
                 return null;
         }
